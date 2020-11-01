9f8bcf3cf01dbe8fc2051a45c787b44b7beeeacf
shiva krishna vara prasad <shivakrishnavaraprasad@outlook.com>
2020-10-31 22:25:48 -0700

    initial commit

---
+++ mb_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\django projects\dj_4_beginners\app_3_message_board\mb_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
